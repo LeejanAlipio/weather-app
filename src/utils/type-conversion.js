e4b03a803a026b800c4d88cbfc9986eee1a4506c
Lee <alipioml0131@gmail.com>
2026-09-02 15:32:02 +0800

    fix: round temperature conversion result to two decimal places

--- src/utils/type-conversion.js
+++ src/utils/type-conversion.js
@@ -2,7 +2,7 @@ import state from './state.js';
 
 export default function getTempType(temp) {
   if (state.temp === 'celcius') {
-    return ((temp - 32) * 5) / 9;
+    return (((temp - 32) * 5) / 9).toFixed(2);
   } else {
     return temp;
   }
