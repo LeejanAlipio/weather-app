359c3d87e4836a947bddaba17ecc3474cbc03016
Lee <alipioml0131@gmail.com>
2026-09-02 15:16:37 +0800

    feat: add elements utility for DOM manipulation in weather app

---
+++ src/utils/elements.js
@@ -0,0 +1,21 @@
+const elements = {
+  input: {
+    searchInput: document.querySelector('.search'),
+  },
+  display: {
+    location: document.querySelector('.location'),
+    description: document.querySelector('.description'),
+    icon: document.querySelector('.weather-icon'),
+    temp: document.querySelector('.temp'),
+    humidity: document.querySelector('.humidity-data'),
+    windSpeed: document.querySelector('.wind-speed-data'),
+    sunrise: document.querySelector('.sunrise-data'),
+    sunset: document.querySelector('.sunset-data'),
+  },
+  buttons: {
+    celcius: document.querySelector('.celcius-btn'),
+    fahrenheit: document.querySelector('.fahrenheit-btn'),
+  },
+};
+
+export default elements;
