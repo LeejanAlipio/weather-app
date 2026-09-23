4d0a89a7ddc6a3f1e163f6973f086c07f4b37197
Lee <alipioml0131@gmail.com>
2026-09-24 00:24:29 +0800

    feat: implement renderWeather function and error handling

---
+++ src/ui/renderWeather.js
@@ -0,0 +1,52 @@
+import { weatherService } from '../services/weatherService.js';
+import { setTemp, getCurrentTemp } from '../state/state.js';
+import { elements } from './elements.js';
+import { capitalize } from '../utils/capitalize.js';
+import { getTemp } from '../utils/tempConversion.js';
+
+export const renderWeather = async (city) => {
+  if (!city) return;
+
+  try {
+    const weatherData = await weatherService.getWeather(city);
+
+    if (!weatherData) {
+      renderErrorMessage('City not Found');
+      return;
+    }
+
+    setTemp(weatherData.temp);
+
+    try {
+      const icon = await import(`../assets/icons/${weatherData.icon}.svg`);
+      elements.display.icon.src = icon.default || '';
+    } catch (error) {
+      console.error(error);
+    }
+
+    elements.display.description.textContent = weatherData.description;
+    elements.display.humidity.textContent = weatherData.humidity;
+    elements.display.location.textContent = capitalize(city);
+    elements.display.sunrise.textContent = weatherData.sunrise;
+    elements.display.sunset.textContent = weatherData.sunset;
+    renderTemp();
+    elements.display.windSpeed.textContent = weatherData.windspeed;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const renderTemp = () => {
+  elements.display.temp.textContent = `${getTemp(getCurrentTemp())}°`;
+};
+
+const renderErrorMessage = (message) => {
+  elements.display.description.textContent = message;
+  elements.display.humidity.textContent = message;
+  elements.display.icon.textContent = '';
+  elements.display.location.textContent = message;
+  elements.display.sunrise.textContent = message;
+  elements.display.sunset.textContent = message;
+  elements.display.temp.textContent = message;
+  elements.display.windSpeed.textContent = message;
+};
