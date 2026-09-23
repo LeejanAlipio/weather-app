372248fcbce99a72468335d353cf2f1400a28e19
Lee <alipioml0131@gmail.com>
2026-09-23 22:46:48 +0800

    Move elements to ui folder

---
+++ src/ui/elements.js
@@ -0,0 +1,19 @@
+export const elements = {
+  input: {
+    searchInput: document.querySelector('.search'),
+  },
+  display: {
+    location: document.querySelector('.location'),
+    description: document.querySelector('.description'),
+    icon: document.querySelector('.weather-icon'),
+    temp: document.querySelector('.temp'),
+    humidity: document.querySelector('.humidity-data'),
+    windSpeed: document.querySelector('.wind-speed-data'),
+    sunrise: document.querySelector('.sunrise-data'),
+    sunset: document.querySelector('.sunset-data'),
+  },
+  buttons: {
+    celsius: document.querySelector('.celsius-btn'),
+    fahrenheit: document.querySelector('.fahrenheit-btn'),
+  },
+};
